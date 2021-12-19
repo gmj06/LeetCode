44ae3d2019fed98a9642ff911c9c4c016b434d31
gmj06 <gowrimj2006@gmail.com>
2021-12-18 19:55:29 -0800

    Algorithm1 - Day1

---
+++ JavaScript/Easy/35.SearchInsertPosition.js
@@ -0,0 +1,48 @@
+/*
+    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+    
+
+    Example 1:
+
+    Input: nums = [1,3,5,6], target = 5
+    Output: 2
+    Example 2:
+
+    Input: nums = [1,3,5,6], target = 2
+    Output: 1
+    Example 3:
+
+    Input: nums = [1,3,5,6], target = 7
+    Output: 4
+    
+
+    Constraints:
+
+    1 <= nums.length <= 104
+    -104 <= nums[i] <= 104
+    nums contains distinct values sorted in ascending order.
+    -104 <= target <= 104
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var searchInsert = function(nums, target) {
+    return searchInsertRecursion(nums, target, 0, nums.length - 1)
+};
+
+var searchInsertRecursion = function(nums, target, first_index, last_index){
+    if(first_index > last_index) return last_index + 1
+    
+    var mid_index = first_index + Math.floor((last_index - first_index)/2);
+    
+    if(nums[mid_index] === target) return mid_index;
+    else if(nums[mid_index] > target) return searchInsertRecursion(nums, target, first_index, mid_index - 1);
+    else return searchInsertRecursion(nums, target, mid_index + 1, last_index);
+}
---
+++ JavaScript/Easy/704.BinarySearch_Recursion.js
@@ -0,0 +1,46 @@
+/*
+    DESCRIPTION 
+    Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+    You must write an algorithm with O(log n) runtime complexity.
+
+    
+
+    Example 1:
+
+    Input: nums = [-1,0,3,5,9,12], target = 9
+    Output: 4
+    Explanation: 9 exists in nums and its index is 4
+    Example 2:
+
+    Input: nums = [-1,0,3,5,9,12], target = 2
+    Output: -1
+    Explanation: 2 does not exist in nums so return -1
+    
+
+    Constraints:
+
+    1 <= nums.length <= 104
+    -104 < nums[i], target < 104
+    All the integers in nums are unique.
+    nums is sorted in ascending order.
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+    return searchRecursion(nums, target, 0,  nums.length - 1)
+};
+
+var searchRecursion = function(nums, target, start_index, end_index){
+    if(start_index > end_index) return -1;
+    
+    mid_index = start_index + Math.floor((end_index - start_index)/2)
+    
+    if(nums[mid_index] === target) return mid_index
+    else if(nums[mid_index] > target) return searchRecursion(nums, target, start_index, mid_index-1)
+    else return searchRecursion(nums, target, mid_index+1, end_index)
+}
